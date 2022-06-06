31b476194b11f88818fec3b5fce37398b0803e6d
Farhan Almasyhur <51041665+FarhanAlmasyhur@users.noreply.github.com>
2022-06-06 21:42:30 +0700

    chore: initial commit

---
+++ Cehat/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cehat
+//
+//  Created by Muhammad Farhan Almasyhur on 10/06/22.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
