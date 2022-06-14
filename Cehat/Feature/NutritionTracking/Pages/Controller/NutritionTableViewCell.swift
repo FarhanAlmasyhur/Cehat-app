b88e03ae29bb42752647fc09f1541a8787e773ce
William-Yulio <william.yulio331@gmail.com>
2022-06-14 16:12:32 +0700

    feat: add nutrition table view

---
+++ Cehat/Feature/NutritionTracking/Pages/Controller/NutritionTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  NutritionTableViewCell.swift
+//  Cehat
+//
+//  Created by William Yulio on 13/06/22.
+//
+
+import UIKit
+
+class NutritionTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var nutritionIconImage: UIImageView!
+    @IBOutlet weak var nutritionLabel: UILabel!
+    @IBOutlet weak var barProgressNutrition: UIProgressView!
+    @IBOutlet weak var barProgressPercentage: UILabel!
+    @IBOutlet weak var nutritionDescription: UILabel!
+    
+    
+    static func nib() -> UINib{
+        return UINib(nibName: "NutritionCell", bundle: nil)
+    }
+    
+    public func configure(with imageName: String){
+        nutritionIconImage.image = UIImage(named: imageName)
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Cehat/Feature/NutritionTracking/Pages/Controller/NutritionViewController.swift
+++ Cehat/Feature/NutritionTracking/Pages/Controller/NutritionViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class NutritionViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(NutritionTableViewCell.nib(), forCellReuseIdentifier: "NutritionCell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NutritionCell", for: indexPath) as! NutritionTableViewCell
+        cell.configure(with: "icon-karbo")
+        return cell
+    }
 
 }
 
