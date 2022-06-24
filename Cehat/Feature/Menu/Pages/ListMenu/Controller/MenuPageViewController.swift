97c0a4d9bfa7e5c80b8dec5c6af4b68cce7bb8e3
Arief Ramadhan <arief.ramadhan43@gmail.com>
2022-06-24 15:36:45 +0700

    merge: listmenu page with master
    * collection menu list page
    
    * segmented control changes
    
    * move page push navigation
    
    * search bar collection view + regroup
    
    * regroup
    
    * fix title

---
+++ Cehat/Feature/Menu/Pages/ListMenu/Controller/MenuPageViewController.swift
@@ -0,0 +1,69 @@
+//
+//  MenuPageViewController.swift
+//  Cehat
+//
+//  Created by Arief Ramadhan on 16/06/22.
+//
+
+import UIKit
+
+class MenuPageViewController: UIViewController {
+    
+    var menuImages:[String] = ["Stim_Ikan","Tumis_Kangkung","Nasi_Goreng","Salad","Salad","Salad"]
+    var menuNames:[String] = ["Nasi Goreng","Baso Sapi","Indomie Goreng","Indomie Rebus","Indomie Rebus","Indomie Rebus"]
+    var viewTotalCalory:[String] = ["100gr","50gr","40gr","80gr","80gr","80gr"]
+    var viewTotalCarbo:[String] = ["100gr","50gr","40gr","80gr","80gr","80gr"]
+    var viewTotalProtein:[String] = ["100gr","50gr","40gr","80gr","80gr","80gr"]
+    var viewTotalFat:[String] = ["100gr","50gr","40gr","80gr","80gr","80gr"]
+    
+    var filteredMenuNames: [String]!
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var menuCollectionView: UICollectionView!
+    @IBOutlet weak var viewSegmented: UIView!
+    @IBOutlet weak var nameEmpty: UILabel!
+    @IBOutlet weak var imageEmpty: UIImageView!
+    @IBAction func didSegmentedTap(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            self.nameEmpty.isHidden = true
+            self.imageEmpty.isHidden = true
+            self.menuCollectionView.isHidden = false
+        }
+        if sender.selectedSegmentIndex == 1 {
+            self.nameEmpty.isHidden = false
+            self.imageEmpty.isHidden = false
+            self.menuCollectionView.isHidden = true
+            nameEmpty.text = "Anda belum menambahkan menu ke favorit"
+            imageEmpty.image = UIImage(systemName: "heart")
+            
+            viewSegmented.backgroundColor = .white
+        }
+        if sender.selectedSegmentIndex == 2 {
+            self.nameEmpty.isHidden = false
+            self.imageEmpty.isHidden = false
+            self.menuCollectionView.isHidden = true
+            nameEmpty.text = "Anda belum menambahkan menu harian, ayo mulai memberikan menu harian untuk memenuhi kebutuhan nutrisi anak"
+            imageEmpty.image = UIImage(systemName: "clock.arrow.circlepath")
+            
+            viewSegmented.backgroundColor = .white
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //sarchbar
+        filteredMenuNames = menuNames
+        searchBar.searchTextField.layer.cornerRadius = 20
+        searchBar.searchTextField.layer.masksToBounds = true
+        searchBar.delegate = self
+        
+        //regis xib
+        let nibCell = UINib(nibName: "MenuPageCollectionViewCell", bundle: nil)
+        menuCollectionView.register(nibCell, forCellWithReuseIdentifier: "cell")
+    }
+//    override func viewWillAppear(_ animated: Bool) {
+//            navigationController?.setNavigationBarHidden(true, animated: animated)
+//        }
+}
