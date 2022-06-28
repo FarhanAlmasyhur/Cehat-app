5aec91967a1d8c7c2ca5b3e19cd497902136e697
Farhan Almasyhur <51041665+FarhanAlmasyhur@users.noreply.github.com>
2022-06-28 22:34:23 +0700

    feat: fixed list menu page (constraint layout and section in Riwayat) (#10)
    Co-authored-by: William-Yulio <william.yulio331@gmail.com>

--- Cehat/Feature/Menu/Pages/ListMenu/Controller/MenuPageCollectionViewCell.swift
+++ Cehat/Feature/Menu/Pages/ListMenu/Controller/MenuPageCollectionViewCell.swift
@@ -19,16 +19,42 @@ class MenuPageCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var labelProtein: UILabel!
     @IBOutlet weak var totalFat: UILabel!
     @IBOutlet weak var labelFat: UILabel!
+    @IBOutlet weak var addButton: UIButton!
     
     
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        labelFat.text = "Lemak"
-        labelCarbo.text = "Karbohidrat"
+        setupLayout()
+    }
+    
+    func setupLayout(){
+        labelFat.text = "lemak"
+        labelFat.textColor = UIColor(hex: 0x507B3E)
+        labelFat.font = UIFont.systemFont(ofSize: 10)
+        labelFat.frame = CGRect(x: 318, y: 327, width: 29, height: 12)
+        labelFat.textAlignment = .center
+        
+        labelCarbo.text = "karbohidrat"
+        labelCarbo.textColor = UIColor(hex: 0x507B3E)
+        labelCarbo.font = UIFont.systemFont(ofSize: 10)
+        labelCarbo.frame = CGRect(x: 175, y: 327, width: 56, height: 12)
+        labelCarbo.textAlignment = .center
+        
         labelCalory.text = "kkal"
-        labelProtein.text = "Protein"
+        labelCalory.textColor = UIColor(hex: 0xE05C23)
+        labelCalory.font = UIFont.systemFont(ofSize: 10)
+        labelCalory.frame = CGRect(x: 137, y: 327, width: 20, height: 12)
+        labelCalory.textAlignment = .center
+        
+        labelProtein.text = "protein"
+        labelProtein.textColor = UIColor(hex: 0x507B3E)
+        labelProtein.font = UIFont.systemFont(ofSize: 10)
+        labelProtein.frame = CGRect(x: 252, y: 327, width: 35, height: 12)
+        labelProtein.textAlignment = .center
+        
+        addButton.tintColor =  UIColor(hex: 0xE88429)
     }
 
 }
